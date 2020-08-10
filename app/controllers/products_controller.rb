132b59c1f1bac9eea1bc4c8d3c4b2b4388036963
Sevda Amini <sevda.amini@hotmail.com>
2020-08-10 11:11:40 +1000

    debugging is going on

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,27 +1,26 @@
 class ProductsController < ApplicationController
   before_action :find_product, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, only: [:new, :edit]
 
   # GET /products
   # GET /products.json
   def index
-    if params[:category].blank?
-              @products = Product.all
-    else
-      @category_id = Category.find_by(name: params[:category]).id
-      @products = Product.where(:category_id => @category_id).order("created_at_DESC")
-    end
+        if params[:category].blank?
+              @products = Product.all.order("created_at DESC")
+        else
+              @category_id = Category.find_by(name: params[:category]).id
+              @products = Product.where(:category_id => @category_id).order("created_at_DESC")
+        end
   end
 
  
   def show
 
-    if @product.reviews.blank?
-			@average_review = 0
-		else
-			@average_review = @product.reviews.average(:rating).round(2)
-    end
-    
+        if @product.reviews.blank?
+                @average_review = 0
+        else
+          @average_review = @product.reviews.average(:rating).round(2)
+        end
   end
  
   def new
@@ -30,16 +29,13 @@ class ProductsController < ApplicationController
     @categories = Category.all.map{ |c| [c.name, c.id] }
   end
 
-  
-  def edit
-   @categories = Category.all.map{ |c| [c.name, c.id] }
-  end
 
-  
-  def create
+
+   def create
     @product = current_user.products.build(product_params)
     @product.category_id = params[:category_id]
     #@product.user_id = current_user.id
+    @categories=Category.pluck(:name, :category_id)
 
       if @product.save
         redirect_to root_path
@@ -47,19 +43,20 @@ class ProductsController < ApplicationController
         render 'new'
       end
   end
+  
+  def edit
+   @categories = Category.all.map{ |c| [c.name, c.id] }
+  end
 
-  # PATCH/PUT /products/1
-  # PATCH/PUT /products/1.json
+  
   def update
-    respond_to do |format|
+    
       @product.category_id = params[:category_id]
       if @product.update(product_params)
-        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
-        format.json { render :show, status: :ok, location: @product }
+       redirect_to product_path(@product)
       else
         render 'edit'
       end
-    end
   end
 
   # DELETE /products/1
@@ -71,14 +68,14 @@ class ProductsController < ApplicationController
 
   private
 
-  def product_params
-    params.require(:product).permit(:name, :description, :price, :category_id, :product_img, )
-    
-  end
+        def product_params
+          params.require(:product).permit(:name, :description, :price, :category_id, :product_img, )
+          
+        end
 
-  def find_product
-    @product = Product.find(params[:id])
-    
-  end
+        def find_product
+          @product = Product.find(params[:id])
+          
+        end
   
 end
